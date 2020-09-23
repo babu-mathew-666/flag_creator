b13bb7c972a03925035dc747e0e8ca0ae95b12cc
sreekanth <sreekanthroot@gmail.com>
2020-09-23 18:07:46 +0530

    first

---
+++ to_flag_format.py
@@ -0,0 +1,17 @@
+flag_to_convert = input('Enter the string to convert to flag: ')
+
+def convert(flag):
+    result = ''
+    for i in flag:
+        if i == 'o' or i == 'O':
+            result += '0'
+        elif i == 's' or i == 'S':
+            result += '5'
+        elif i == 'g' or i == 'G':
+            result += '6'
+        elif i == ' ':
+            result += '_'
+        else:
+            result += i
+    return result
+print(convert(flag_to_convert))
